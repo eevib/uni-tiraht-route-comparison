ad2c22119ece64b23145dc0af9b129342ae632ae
Melker Narikka <meklu@meklu.org>
2021-08-14 06:39:00 +0300

    JPS:n alkua
    Vielä ei olla kovin lähellä valmista

---
+++ routecomparison/src/main/java/org/meklu/routecomparison/domain/JPS.java
@@ -0,0 +1,273 @@
+
+package org.meklu.routecomparison.domain;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
+/** Toteuttaa Jump Point Search -reitinhakualgoritmin.
+ *
+ * <p>Tämän hakualgoritmin taustalla oleva A*-implementaatio on mukailtu
+ * pseudokoodista, joka on haettu osoitteesta
+ *    https://en.wikipedia.org/wiki/A*_search_algorithm
+ *
+ * <p>Tämän hakualgoritmin varsinainen mielenkiintoinen osuus (oksinta ja hypyt,
+ * engl. kirjallisuudessa "pruning" ja "jumping") on mukailtu artikkelista
+ *    "Online Graph Pruning for Pathfinding on Grid Maps",
+ *       Harabor, Daniel & Grastien, Alban (2011), 25th National Conference on
+ *       Artificial Intelligence. AAAI.
+ */
+public class JPS implements Reitinhakija {
+    private Ruudukko ruudukko;
+    private Heuristiikka heuristiikka;
+    private final double[] suuntienPainot;
+    private final Koordinaatti[] suunnat;
+
+    public JPS(Ruudukko ruudukko, Heuristiikka heuristiikka) {
+        this.ruudukko = ruudukko;
+        this.heuristiikka = heuristiikka;
+
+        double[] naapurienPainot = new double[8];
+        Koordinaatti[] naapurit = new Koordinaatti[8];
+        double diagonaaliPaino = Math.sqrt(2);
+        for (int i = 0, x = -1; x <= 1 && i < naapurit.length; ++x) {
+            for (int y = -1; y <= 1 && i < naapurit.length; ++y) {
+                if (x == 0 && y == 0) {
+                    continue;
+                }
+                if (x != 0 && y != 0) {
+                    naapurienPainot[i] = diagonaaliPaino;
+                } else {
+                    naapurienPainot[i] = 1;
+                }
+                naapurit[i] = new Koordinaatti(x, y);
+                ++i;
+            }
+        }
+        this.suunnat = naapurit;
+        this.suuntienPainot = naapurienPainot;
+    }
+
+    private Koordinaatti[] kokoaReitti(Koordinaatti[][] tulosuunnat, Koordinaatti nykyinen) {
+        LinkedList<Koordinaatti> reitti = new LinkedList<>();
+        while (nykyinen != null) {
+            reitti.addFirst(nykyinen);
+            nykyinen = tulosuunnat[nykyinen.getY()][nykyinen.getX()];
+        }
+        Koordinaatti[] reittiTaulukko = new Koordinaatti[reitti.size()];
+        int i = 0;
+        for (Koordinaatti k : reitti) {
+            reittiTaulukko[i] = k;
+            ++i;
+        }
+        return reittiTaulukko;
+    }
+
+    /** Palauttaa solmun naapurit absoluuttisten koordinaattien listana
+     *
+     * @param solmu Solmu, jonka naapurit halutaan
+     * @return Taulukko naapureita
+     */
+    private Koordinaatti[] naapurit(Koordinaatti solmu) {
+        Koordinaatti[] abs = new Koordinaatti[this.suunnat.length];
+        for (int i = 0; i < this.suunnat.length; ++i) {
+            abs[i] = new Koordinaatti(
+                solmu.getX() + this.suunnat[i].getX(),
+                solmu.getY() + this.suunnat[i].getY()
+            );
+        }
+        return abs;
+    }
+
+    /** Suorittaa oksintaa
+     *
+     * @param  solmu    Solmu, jonka naapureita oksitaan
+     * @param  vanhempi Solmu, josta saavuttiin tähän solmuun
+     * @return Palauttaa taulukon sellaisia naapureita, joita on järkevä
+     *         käsitellä. Vähemmän järkevät naapurit on vain nullattu pois.
+     */
+    public Koordinaatti[] oksi(Koordinaatti solmu, Koordinaatti vanhempi) {
+        Koordinaatti[] oksitut = naapurit(solmu);
+        // mitään ei oksita, jos vanhempaa ei ole
+        if (null == vanhempi) {
+            return oksitut;
+        }
+        int sx = solmu.getX();
+        int sy = solmu.getY();
+        int vx = vanhempi.getX();
+        int vy = vanhempi.getY();
+        int dx = sx - vx;
+        int dy = sy - vy;
+        Koordinaatti naapuri;
+        if (dx == 0 || dy == 0) {
+            // suora siirto
+            //len( [p(x), … , n] \ x ) ≤ len( [p(x), x, n] )
+            for (int i = 0; i < oksitut.length; ++i) {
+                naapuri = oksitut[i];
+                if (naapuri.equals(vanhempi)) {
+                    oksitut[i] = null;
+                    continue;
+                }
+                if (ruudukko.ruutuEstynyt(naapuri.getX(), naapuri.getY())) {
+                    oksitut[i] = null;
+                    continue;
+                }
+                if (dy == 0) { // siirto sivulle
+                    if (sx + dx != naapuri.getX()) {
+                        oksitut[i] = null;
+                        continue;
+                    } else if (sy != naapuri.getY()) {
+                        if (!ruudukko.ruutuEstynyt(sx, naapuri.getY())) {
+                            oksitut[i] = null;
+                            continue;
+                        }
+                    }
+                } else { // dx == 0, siirto ylös/alas
+                    if (sy + dy != naapuri.getY()) {
+                        oksitut[i] = null;
+                        continue;
+                    } else if (sx != naapuri.getX()) {
+                        if (!ruudukko.ruutuEstynyt(naapuri.getX(), sy)) {
+                            oksitut[i] = null;
+                            continue;
+                        }
+                    }
+                }
+            }
+        } else {
+            // diagonaalinen siirto
+            //len( [p(x), … , n] \ x ) < len( [p(x), x, n] )
+            for (int i = 0; i < oksitut.length; ++i) {
+                naapuri = oksitut[i];
+                if (naapuri.equals(vanhempi)) {
+                    oksitut[i] = null;
+                    continue;
+                }
+                if (vx == naapuri.getX()) {
+                    if (sy == naapuri.getY()) {
+                        oksitut[i] = null;
+                        continue;
+                    } else if (sy + dy == naapuri.getY()) {
+                        if (!ruudukko.ruutuEstynyt(vx, sy)) {
+                            oksitut[i] = null;
+                            continue;
+                        }
+                    }
+                }
+                if (vy == naapuri.getY()) {
+                    if (sx == naapuri.getX()) {
+                        oksitut[i] = null;
+                        continue;
+                    } else if (sx + dx == naapuri.getX()) {
+                        if (!ruudukko.ruutuEstynyt(sx, vy)) {
+                            oksitut[i] = null;
+                            continue;
+                        }
+                    }
+                }
+            }
+        }
+        return oksitut;
+    }
+
+    /** Suorittaa JPS-reitinhakualgoritmin asetetussa ruudukossa
+     *
+     * @param lahtoX Lähtöpisteen x-koordinaatti
+     * @param lahtoY Lähtöpisteen y-koordinaatti
+     * @param maaliX Maalin x-koordinaatti
+     * @param maaliY Maalin y-koordinaatti
+     * @return Taulukko koordinaatteja lähdöstä maaliin tai null
+     */
+    @Override
+    public Koordinaatti[] etsiReitti(int lahtoX, int lahtoY, int maaliX, int maaliY) {
+        // Paetaan sen kummemmitta, mikäli koordinaatit ruudukon ulkopuolella
+        if (!this.ruudukko.ruudukonSisalla(maaliX, maaliY) || !this.ruudukko.ruudukonSisalla(lahtoX, lahtoY)) {
+            return null;
+        }
+
+        // Sisältää mahdollisesti läpikäyntiä tarvitsevat solmut minimikekona
+        // Parin a-komponentin suhteen. Parin a-komponenttina on siihen
+        // täsmäävän solmun paras hinta-arvio.
+        PriorityQueue<Pari<Double, Koordinaatti>> avoimetSolmut =
+                new PriorityQueue<>(
+                    (pA, pB) -> pA.getA().compareTo(pB.getA())
+                );
+        // aluksi sisältää vain lähtösolmun
+        avoimetSolmut.add(new Pari<>(0.0, new Koordinaatti(lahtoX, lahtoY)));
+        // ja avustava hakutaulu solmujen esiintymiseen edeltävässä tietorakenteessa
+        boolean[][] avoimissaSolmuissa = new boolean[this.ruudukko.getKorkeus()][this.ruudukko.getLeveys()];
+        for (int y = 0; y < this.ruudukko.getKorkeus(); ++y) {
+            for (int x = 0; x < this.ruudukko.getLeveys(); ++x) {
+                avoimissaSolmuissa[y][x] = false;
+            }
+        }
+
+        // Kätevä hakutaulu tulosuunnallemme. Sisältää käsiteltävän polun
+        // jokaista koordinaattia (x,y) vastaavan sitä edeltäneen koordinaatin
+        // kohdassa tulosuunnat[y][x].
+        Koordinaatti[][] tulosuunnat = new Koordinaatti[this.ruudukko.getKorkeus()][this.ruudukko.getLeveys()];
+        for (int y = 0; y < this.ruudukko.getKorkeus(); ++y) {
+            for (int x = 0; x < this.ruudukko.getLeveys(); ++x) {
+                tulosuunnat[y][x] = null;
+            }
+        }
+
+        // Solmulle (x,y) sisältää kohdassa [y][x] halvimman tähän mennessä
+        // tunnetun lähdöstä solmuun (x,y) vievän polun
+        double[][] halvinReittiTahan = new double[this.ruudukko.getKorkeus()][this.ruudukko.getLeveys()];
+        for (int y = 0; y < this.ruudukko.getKorkeus(); ++y) {
+            for (int x = 0; x < this.ruudukko.getLeveys(); ++x) {
+                halvinReittiTahan[y][x] = Double.POSITIVE_INFINITY;
+            }
+        }
+        halvinReittiTahan[lahtoY][lahtoX] = 0;
+
+        // Solmulle (x,y) sisältää arvon halvinReittiTahan[y][x] + heuristiikka(x, y, maaliX, maaliY).
+        // Tämä sisältää toisin sanoen nykyisen parhaan hinta-arvion polulle.
+        double[][] halvinReittiMaaliin = new double[this.ruudukko.getKorkeus()][this.ruudukko.getLeveys()];
+        for (int y = 0; y < this.ruudukko.getKorkeus(); ++y) {
+            for (int x = 0; x < this.ruudukko.getLeveys(); ++x) {
+                halvinReittiMaaliin[y][x] = Double.POSITIVE_INFINITY;
+            }
+        }
+        halvinReittiMaaliin[lahtoY][lahtoX] = this.heuristiikka.lyhinMahdollinenEtaisyys(lahtoX, lahtoY, maaliX, maaliY);
+
+        Koordinaatti maali = new Koordinaatti(maaliX, maaliY);
+        Koordinaatti nykyinen = null;
+
+        while (null != avoimetSolmut.peek()) {
+            nykyinen = avoimetSolmut.poll().getB();
+            if (nykyinen.equals(maali)) {
+                return this.kokoaReitti(tulosuunnat, nykyinen);
+            }
+            int nykyinenX = nykyinen.getX();
+            int nykyinenY = nykyinen.getY();
+            avoimissaSolmuissa[nykyinenY][nykyinenX] = false;
+
+            for (int i = 0; i < suunnat.length; ++i) {
+                int naapuriX = nykyinenX + suunnat[i].getX();
+                int naapuriY = nykyinenY + suunnat[i].getY();
+                double paino = suuntienPainot[i];
+                Koordinaatti naapuri = new Koordinaatti(naapuriX, naapuriY);
+                if (this.ruudukko.ruutuEstynyt(naapuriX, naapuriY)) {
+                    continue;
+                }
+                double mahdollinenHalvinReittiTahan = halvinReittiTahan[nykyinenY][nykyinenX] + paino;
+                if (mahdollinenHalvinReittiTahan < halvinReittiTahan[naapuriY][naapuriX]) {
+                    tulosuunnat[naapuriY][naapuriX] = nykyinen;
+                    halvinReittiTahan[naapuriY][naapuriX] = mahdollinenHalvinReittiTahan;
+                    halvinReittiMaaliin[naapuriY][naapuriX] = mahdollinenHalvinReittiTahan + this.heuristiikka.lyhinMahdollinenEtaisyys(naapuriX, naapuriY, maaliX, maaliY);
+                    if (!avoimissaSolmuissa[naapuriY][naapuriX]) {
+                        avoimetSolmut.add(new Pari<>(halvinReittiMaaliin[naapuriY][naapuriX], naapuri));
+                        avoimissaSolmuissa[naapuriY][naapuriX] = true;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void asetaRuudukko(Ruudukko ruudukko) {
+        this.ruudukko = ruudukko;
+    }
+}
---
+++ routecomparison/src/test/java/JPSTest.java
@@ -0,0 +1,222 @@
+
+import java.util.Arrays;
+import org.meklu.routecomparison.domain.*;
+import org.meklu.routecomparison.util.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class JPSTest {
+    private Ruudukko ruudukko;
+    private Heuristiikka heuristiikka;
+    private JPS jps;
+
+    @Before
+    public void setUp() throws Exception {
+        this.ruudukko = new Ruudukko(6, 4);
+        this.heuristiikka = new Heuristiikka();
+        this.jps = new JPS(this.ruudukko, this.heuristiikka);
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void tyhjanRuudukonReittiOptimimittainen() {
+        Koordinaatti[] reitti = jps.etsiReitti(0, 0, this.ruudukko.getLeveys() - 1, this.ruudukko.getKorkeus() - 1);
+        assertNotNull(reitti);
+        assertEquals(6, reitti.length);
+        Reitintutkija rt = new Reitintutkija(ruudukko, reitti);
+        assertEquals(rt.getReitinPituus(), heuristiikka.lyhinMahdollinenEtaisyys(0, 0, this.ruudukko.getLeveys() - 1, this.ruudukko.getKorkeus() - 1), 0.0001);
+        assertEquals(false, rt.onkoReitissaOngelmia());
+    }
+
+    @Test
+    public void nullJosRuudukonUlkopuolinenLahtopiste() {
+        Koordinaatti[] reitti = jps.etsiReitti(-1, -1, this.ruudukko.getLeveys() - 1, this.ruudukko.getKorkeus() - 1);
+        assertNull(reitti);
+    }
+
+    @Test
+    public void nullJosRuudukonUlkopuolinenMaalipiste() {
+        Koordinaatti[] reitti = jps.etsiReitti(0, 0, this.ruudukko.getLeveys(), this.ruudukko.getKorkeus());
+        assertNull(reitti);
+    }
+
+    @Test
+    public void yksinkertaisenRuudukonReittiOnOptimi() {
+        /*
+          | @ * - - - - |
+          | @ * - - - - |
+          | @ * - - - - |
+          | - @ @ @ @ @ |
+          |Reitin pituus on 7.414213562373095, solmuja 8.
+        */
+        for (int y = 0; y < ruudukko.getKorkeus() - 1; ++y) {
+            ruudukko.asetaEste(true, 1, y);
+        }
+        Koordinaatti[] reitti = jps.etsiReitti(0, 0, this.ruudukko.getLeveys() - 1, this.ruudukko.getKorkeus() - 1);
+        assertNotNull(reitti);
+        Reitintutkija rt = new Reitintutkija(ruudukko, reitti);
+        assertEquals(7.4142, rt.getReitinPituus(), 0.0001);
+        assertEquals(8, rt.getReitissaSolmuja());
+        assertEquals(false, rt.onkoReitissaOngelmia());
+    }
+
+    @Test
+    public void hienommanRuudukonReittiOnOptimi() {
+        /*
+          | @ * - @ - - |
+          | @ * @ * @ - |
+          | @ * @ * @ - |
+          | - @ - * - @ |
+          |Reitin pituus on 11.071067811865476, solmuja 10.
+        */
+        for (int y = 0; y < ruudukko.getKorkeus() - 1; ++y) {
+            ruudukko.asetaEste(true, 1, y);
+        }
+        for (int y = 1; y < ruudukko.getKorkeus(); ++y) {
+            ruudukko.asetaEste(true, 3, y);
+        }
+        Koordinaatti[] reitti = jps.etsiReitti(0, 0, this.ruudukko.getLeveys() - 1, this.ruudukko.getKorkeus() - 1);
+        assertNotNull(reitti);
+        Reitintutkija rt = new Reitintutkija(ruudukko, reitti);
+        assertEquals(11.0710, rt.getReitinPituus(), 0.0001);
+        assertEquals(10, rt.getReitissaSolmuja());
+        assertEquals(false, rt.onkoReitissaOngelmia());
+    }
+
+    @Test
+    public void oksiIlmanVanhempaaKaikkiNaapuritMukana() {
+        Koordinaatti vanhempi = null;
+        Koordinaatti solmu = new Koordinaatti(1, 1);
+        Koordinaatti[] oksitut = jps.oksi(solmu, vanhempi);
+        Koordinaatti[] odotetut = {
+            new Koordinaatti(0, 0),
+            new Koordinaatti(0, 1),
+            new Koordinaatti(0, 2),
+            new Koordinaatti(1, 0),
+            new Koordinaatti(1, 2),
+            new Koordinaatti(2, 0),
+            new Koordinaatti(2, 1),
+            new Koordinaatti(2, 2)
+        };
+        assertArrayEquals(odotetut, oksitut);
+    }
+
+    @Test
+    public void oksiVaakasuoranLuonnollisetNaapuritMukana() {
+        Koordinaatti vanhempi = new Koordinaatti(0, 0);
+        Koordinaatti solmu = new Koordinaatti(1, 0);
+        Koordinaatti[] oksitut = jps.oksi(solmu, vanhempi);
+        Koordinaatti[] odotetut = {
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            new Koordinaatti(2, 0),
+            null
+        };
+        assertArrayEquals(odotetut, oksitut);
+    }
+
+    @Test
+    public void oksiPystysuoranLuonnollisetNaapuritMukana() {
+        Koordinaatti vanhempi = new Koordinaatti(0, 0);
+        Koordinaatti solmu = new Koordinaatti(0, 1);
+        Koordinaatti[] oksitut = jps.oksi(solmu, vanhempi);
+        Koordinaatti[] odotetut = {
+            null,
+            null,
+            null,
+            null,
+            new Koordinaatti(0, 2),
+            null,
+            null,
+            null
+        };
+        assertArrayEquals(odotetut, oksitut);
+    }
+
+    @Test
+    public void oksiDiagonaalinLuonnollisetNaapuritMukana() {
+        Koordinaatti vanhempi = new Koordinaatti(0, 0);
+        Koordinaatti solmu = new Koordinaatti(1, 1);
+        Koordinaatti[] oksitut = jps.oksi(solmu, vanhempi);
+        Koordinaatti[] odotetut = {
+            null,
+            null,
+            null,
+            null,
+            new Koordinaatti(1, 2),
+            null,
+            new Koordinaatti(2, 1),
+            new Koordinaatti(2, 2)
+        };
+        assertArrayEquals(odotetut, oksitut);
+    }
+
+    @Test
+    public void oksiKaanteisDiagonaalinLuonnollisetNaapuritMukana() {
+        Koordinaatti vanhempi = new Koordinaatti(2, 2);
+        Koordinaatti solmu = new Koordinaatti(1, 1);
+        Koordinaatti[] oksitut = jps.oksi(solmu, vanhempi);
+        Koordinaatti[] odotetut = {
+            new Koordinaatti(0, 0),
+            new Koordinaatti(0, 1),
+            null,
+            new Koordinaatti(1, 0),
+            null,
+            null,
+            null,
+            null
+        };
+        assertArrayEquals(odotetut, oksitut);
+    }
+
+    @Test
+    public void oksiVaakasuoranPakotteetMukana() {
+        Koordinaatti vanhempi = new Koordinaatti(0, 1);
+        Koordinaatti solmu = new Koordinaatti(1, 1);
+        ruudukko.asetaEste(true, 1, 0);
+        ruudukko.asetaEste(true, 1, 2);
+        Koordinaatti[] oksitut = jps.oksi(solmu, vanhempi);
+        Koordinaatti[] odotetut = {
+            null,
+            null,
+            null,
+            null,
+            null,
+            new Koordinaatti(2, 0),
+            new Koordinaatti(2, 1),
+            new Koordinaatti(2, 2)
+        };
+        assertArrayEquals(odotetut, oksitut);
+    }
+
+    @Test
+    public void oksiPystysuoranPakotteetMukana() {
+        Koordinaatti vanhempi = new Koordinaatti(1, 0);
+        Koordinaatti solmu = new Koordinaatti(1, 1);
+        ruudukko.asetaEste(true, 0, 1);
+        ruudukko.asetaEste(true, 2, 1);
+        Koordinaatti[] oksitut = jps.oksi(solmu, vanhempi);
+        System.out.println(Arrays.toString(oksitut));
+        Koordinaatti[] odotetut = {
+            null,
+            null,
+            new Koordinaatti(0, 2),
+            null,
+            new Koordinaatti(1, 2),
+            null,
+            null,
+            new Koordinaatti(2, 2)
+        };
+        assertArrayEquals(odotetut, oksitut);
+    }
+}
